763d7338b97da1fa4df3fd7b9a9137c4ebc89dbb
Alex Obukhov <alexobukhov@gmail.com>
2023-04-25 22:57:33 +0300

    Initial commit

---
+++ itactive/src/test/java/ru/gb/inventory/itactive/ItActiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.inventory.itactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItActiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
