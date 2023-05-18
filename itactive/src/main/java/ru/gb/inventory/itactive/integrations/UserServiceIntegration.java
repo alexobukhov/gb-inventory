e81e1dc9d366867cd20e4f6fa6d02733934ec855
bonjourvalery <valeryrayumov@yandex.ru>
2023-05-18 12:12:51 +0300

    Доработки

--- itactive/src/main/java/ru/gb/inventory/itactive/integrations/UserServiceIntegration.java
+++ itactive/src/main/java/ru/gb/inventory/itactive/integrations/UserServiceIntegration.java
@@ -2,10 +2,13 @@ package ru.gb.inventory.itactive.integrations;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 @RequiredArgsConstructor
 public class UserServiceIntegration {
 
-    private final
+    private final WebClient userServiceWebClient;
+
+
 }
