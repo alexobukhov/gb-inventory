863bbdc15b10cfd2fb2d15f521c33c5181ee2591
MikhailFilippov1 <cp_filippov@mail.ru>
2023-06-15 17:21:51 +0300

    Доделал UserServiceIntegrations и добавил UserDto

--- department/src/main/java/ru/gb/inventory/department/controllers/DepartmentController.java
+++ department/src/main/java/ru/gb/inventory/department/controllers/DepartmentController.java
@@ -10,7 +10,7 @@ import ru.gb.inventory.department.api.ResourceNotFoundException;
 import ru.gb.inventory.department.converters.DepartmentConverter;
 import ru.gb.inventory.department.entyties.Department;
 import ru.gb.inventory.department.exceptions.DataValidationException;
-import ru.gb.inventory.department.services.implementations.DepartmentServiceImpl;
+import ru.gb.inventory.department.services.DepartmentService;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -19,17 +19,17 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/departments")
 public class DepartmentController {
-    private final DepartmentServiceImpl departmentServiceImpl;
+    private final DepartmentService departmentService;
     private final DepartmentConverter departmentConverter;
 
     @GetMapping
     public List<DepartmentDto> findAll(){
-        return departmentServiceImpl.findAll();
+        return departmentService.findAll();
     }
 
     @GetMapping("/{id}")
     public DepartmentDto findById(@PathVariable Long id){
-        Department department = departmentServiceImpl.findById(id).orElseThrow(() -> new ResourceNotFoundException("Department id: " + id + " not found"));
+        Department department = departmentService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Department id: " + id + " not found"));
         return departmentConverter.entityToDto(department);
     }
 
@@ -38,17 +38,17 @@ public class DepartmentController {
         if(bindingResult.hasErrors()){
             throw new DataValidationException(bindingResult.getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.toList()));
         }
-        Department department = departmentServiceImpl.createNewDepartment(departmentDto);
+        Department department = departmentService.createNewDepartment(departmentDto);
         return departmentConverter.entityToDto(department);
     }
 
     @PutMapping
     public void updateDepartment(@RequestBody DepartmentDto departmentDto){
-        departmentServiceImpl.updateDepartmentFromDto(departmentDto);
+        departmentService.updateDepartmentFromDto(departmentDto);
     }
 
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable Long id){
-        departmentServiceImpl.deleteById(id);
+        departmentService.deleteById(id);
     }
 }
--- department/src/main/java/ru/gb/inventory/department/converters/DepartmentConverter.java
+++ department/src/main/java/ru/gb/inventory/department/converters/DepartmentConverter.java
@@ -9,18 +9,15 @@ import ru.gb.inventory.department.entyties.Department;
 @Component
 @AllArgsConstructor
 public class DepartmentConverter {
-//    private final UserService userService;
 
     public DepartmentDto entityToDto(Department department){
-        return new DepartmentDto(department.getId(), department.getTitle() /*, department.getUser().getId()*/);
+        return new DepartmentDto(department.getId(), department.getTitle());
     }
 
     public Department dtoToEntity(DepartmentDto departmentDto){
         Department department = new Department();
         department.setId(departmentDto.getId());
         department.setTitle(departmentDto.getTitle());
-//        User user = userService.findByTitle(departmentDto.getTitle()).orElseThrow(() -> new ResourceNotFoundException("Пользователь не найден"));
-//        department.setUser(user);
         return department;
     }
 }
