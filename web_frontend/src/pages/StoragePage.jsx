4905ba18360f047e0033c2c0e802d779bf6bd88c
Liss <les.karo@yandex.ru>
2023-06-25 15:31:29 +0400

    Взаимодействие с сервером, отрисовка device

--- web_frontend/src/pages/StoragePage.jsx
+++ web_frontend/src/pages/StoragePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import NavbarStorage from "../components/UI/Navbar/NavbarStorage";
 import cl from "./DevicePage/DevicePage.module.css";
 import {Card, Container} from "react-bootstrap";
@@ -8,8 +8,30 @@ import StorageMenu from "../components/DeviceComponent/StorageMenu";
 import Form from "react-bootstrap/Form";
 import BrandBar from "../components/DeviceComponent/BrandBar";
 import ConditionBar from "../components/DeviceComponent/ConditionBar";
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
+import {fetchBrands, fetchConditions, fetchDevices, fetchTypes} from "../http/deviceAPI";
 
-const StoragePage = () => {
+const StoragePage = observer(() => {
+    const {device} = useContext(Context)
+
+    useEffect(() => {
+        fetchTypes().then(data => device.setTypes(data))
+        fetchBrands().then(data => device.setBrands(data))
+        fetchConditions().then(data => device.setConditions(data))
+        fetchDevices(null, null,  null,1, 3).then(data => {
+            device.setDevices(data.rows)
+            // device.setTotalCount(data.count)
+        })
+    }, [])
+
+    useEffect(() => {
+        fetchDevices(device.selectedType.id, device.selectedBrand.id, device.selectedCondition.id, device.page, 3).then(data => {
+            device.setDevices(data.rows)
+            // device.setTotalCount(data.count)
+        })
+    }, [device.page, device.selectedType, device.selectedBrand, device.selectedCondition.id])
+
     return (
         <Container>
             <NavbarStorage/>
@@ -26,6 +48,6 @@ const StoragePage = () => {
             </Form>
         </Container>
     );
-};
+});
 
 export default StoragePage;
