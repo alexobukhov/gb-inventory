2d669679402fd7191408a7b3488427e64746446a
Liss <les.karo@yandex.ru>
2023-06-25 14:54:21 +0400

    Создание Loader, отрисовка пользователя

--- web_frontend/src/App.js
+++ web_frontend/src/App.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+import {observer} from "mobx-react-lite";
+import {Context} from "./index";
+import {check} from "./http/userApi";
+import Loader from "./components/UI/Loader/Loader";
 
 
-function App() {
+const App = observer(() => {
+    const {user} = useContext(Context)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        check().then(data => {
+            user.setUsers(true)
+            user.setIsAuth(true)
+        }).finally(() => setLoading(false))
+    }, [])
+    if (loading) {
+        return <Loader/>
+    }
+
     return (
         <BrowserRouter>
             <AppRouter/>
         </BrowserRouter>
     );
-}
+})
 
 export default App;
