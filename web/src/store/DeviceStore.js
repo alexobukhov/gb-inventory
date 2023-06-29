08057a1d9a54caa11d72ba43b275935708522df3
Liss <les.karo@yandex.ru>
2023-06-29 18:36:29 +0400

    Пути к микросервисам

--- web/src/store/DeviceStore.js
+++ web/src/store/DeviceStore.js
@@ -2,39 +2,10 @@ import {makeAutoObservable} from "mobx";
 
 export default class DeviceStore {
     constructor() {
-        this._types = [
-            {id: 1, name: 'Мышь'},
-            {id: 2, name: 'Клавиатура'},
-            {id: 3, name: 'Принтер'},
-            {id: 4, name: 'Монитор'},
-            {id: 5, name: 'Наушники'},
-            {id: 6, name: 'Ноутбук'},
-        ]
-
-        this._conditions = [
-            {id: 1, name: 'Новый'},
-            {id: 2, name: 'Б/У'},
-            {id: 3, name: 'Брак'},
-        ]
-
-        this._brands = [
-            {id: 1, name: 'Samsung'},
-            {id: 2, name: 'LG'},
-            {id: 3, name: 'Lenovo'},
-            {id: 4, name: 'Asus'},
-            {id: 5, name: 'Aser'},
-
-        ]
-
-        this._devices = [
-            {id: 1, name: '25BK550Y', SN: '910NTGY2X975', condition_id: 'Б/У'},
-            {id: 2, name: 'UX310U', SN: 'J5N0CV06W555209', condition_id: 'неисправное'},
-            {id: 3, name: 'ZBook G5', SN: '5CG9290M8M', condition_id: 'новое'},
-            {id: 1, name: '25BK550Y', SN: '910NTGY2X975', condition_id: 'Б/У'},
-            {id: 2, name: 'UX310U', SN: 'J5N0CV06W555209', condition_id: 'неисправное'},
-            {id: 3, name: 'ZBook G5', SN: '5CG9290M8M', condition_id: 'новое'},
-        ]
-
+        this._types = []
+        this._conditions = []
+        this._brands = []
+        this._devices = []
         this._selectedType = {}
         this._selectedBrand = {}
         this._selectedCondition = {}
--- web/src/store/UserStore.js
+++ web/src/store/UserStore.js
@@ -2,61 +2,9 @@ import {makeAutoObservable} from "mobx";
 
 export default class UserStore {
     constructor() {
-        this._users = [
-            {
-                id: 1,
-                name: 'Иван',
-                lastname: 'Иванов',
-                middlename: 'Иванович',
-                grade: 1,
-                email: 'ivanov@email.com',
-                role: 'Admin'
-            },
-            {
-                id: 2,
-                name: 'Петр',
-                lastname: 'Петров',
-                middlename: 'Петрович',
-                grade: 3,
-                email: 'petrov@email.com',
-                role: 'Storage'
-            },
-            {
-                id: 3,
-                name: 'Клим',
-                lastname: 'Пупкин',
-                middlename: 'Александрович',
-                grade: 2,
-                email: 'pupkin@email.com',
-                role: 'User'
-            },
-            {
-                id: 4,
-                name: 'Алексей',
-                lastname: 'Сидоров',
-                middlename: 'Сергеевич',
-                grade: 1,
-                email: 'sidorov@email.com',
-                role: 'User'
-            },
-
-        ]
-
-        this._jobs = [
-            {id: 1, name: 'Водитель'},
-            {id: 2, name: 'Кладовщик'},
-            {id: 3, name: 'Мастер участка'},
-            {id: 4, name: 'Инженер по программированию'},
-            {id: 5, name: 'Инженет технолог'},
-
-        ]
-
-        this._departments = [
-            {id: 1, name: 'Транспортный цех'},
-            {id: 2, name: 'Инструментальный цех'},
-            {id: 3, name: 'Модельный цех'},
-            {id: 3, name: 'Инженерный цех'},
-        ]
+        this._users = []
+        this._jobs = []
+        this._departments = []
         this._isAuth = true
         this._isAdmin = true
         this._isStorage = true
